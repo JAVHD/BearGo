7c68d24b81953a934f3d3790b2830483430bf743
bear <593788753@qq.com>
2018-09-25 16:32:13 +0800

    add single beargo to init route package

---
+++ beargo/beargo.go
@@ -0,0 +1,23 @@
+/*
+* @Author: bear
+* @Date:   2018-09-22 15:57:08
+* @Last Modified by:   bear
+* @Last Modified time: 2018-09-25 16:26:57
+ */
+package beargo
+
+import (
+	_ "beargo/route" //加载路由init
+)
+
+func init() {
+	//fmt.Println("this a core init")
+}
+
+func Run() {
+	//fmt.Println("go frame run")
+	//启动路由
+
+	//IniRoute()
+
+}
